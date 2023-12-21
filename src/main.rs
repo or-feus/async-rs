055e1b1115d2f50c996486498ec41ae6f9951941
or-feus <barca105@nate.com>
2023-12-21 16:58:37 +0900

    Cargo.lock /target .git

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::Arc;
+use tokio::sync::Mutex;
+use tokio;
+
+
+#[tokio::main]
+async fn main() {
+
+
+    let data = Arc::new(Mutex::new(0));
+
+    let mut handles = vec![];
+
+    for _ in 0..10 {
+        let data = Arc::clone(&data);
+
+        for _ in 0..10 {
+            let data_clone = Arc::clone(&data);
+            let handle = tokio::spawn(async move {
+                let mut data = data_clone.lock().await;
+                *data += 1;
+            });
+            handles.push(handle);
+        }
+    }
+
+    for handle in handles {
+        handle.await.unwrap();
+    }
+
+    println!("Result: {}", *data.lock().await)
+
 }
